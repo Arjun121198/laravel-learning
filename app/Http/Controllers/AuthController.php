a873c2468dc0d2687f91fccf488855336be5a412
gotocva <gotocva@gmail.com>
2022-02-04 12:16:01 +0530

    change authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\Register;
 use Illuminate\Http\Request;
 use Auth;
 use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -25,37 +26,59 @@ class AuthController extends Controller
 
     public function registeruser(Request $request)
     {
+        $this->validate($request,[
+             'name' => 'required',
+            'email' => 'required|email|unique:registers',
+            'password' => 'required|alphaNum|min:5|max:12'
+        ]);
+  
         $name = $request->name;
         $email = $request->email;
         $password = $request->password;
          Register::create([
             'name' => $request['name'],
             'email' => $request['email'],
-            'password' => $request['password']
+            'password' => Hash::make($request['password'])
         ]);
-                 return view('login');
+                 return view('home');
     }
     public function loginuser(Request $request)
     {
       $this->validate($request,[
           'email' => 'required|email',
-          'password' => 'required|alphaNum|min:3'
+          'password' => 'required|alphaNum|min:5|max:12'
 
       ]);
 
 
-      $user = Register::where('email',$request->email)->where('password',$request->password)->first();
+      $user = Register::where('email',$request->email)->first();
+       if(!$user)
+       {
+        return back()->with('fail','We do not recognize your email');
+       }
+       else
+       {
 
+       }
+       if(Hash::check($request->password,$user->password))
+       {
+        $request->Session()->put('loginuser',$user->id);
+        return redirect('home');
+       }
+       else
+       {
+           return back()->with('fail','password is incorrect');
 
-      if(isset($user))
-      { 
-          return view('home');
+       }
+    //   if(isset($user))
+    //   { 
+    //       return view('home');
 
-      }
-      else
-      {
-        return back()->with('error','wrong Login Details');
-      }
+    //   }
+    //   else
+    //   {
+    //     return back()->with('error','wrong Login Details');
+    //   }
     }
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.master')
 @section('title','login')
 @section('content')
-<form action="loginuser" method="post">
+<form action="loginuser" autocomplete="off" method="post">
   @csrf
+  @if(Session::get('fail'))
+<div class="alert alert-danger">
+  {{ Session::get('fail') }}
+</div>
+  @endif
 <section class="vh-100">
     <div class="container-fluid h-custom">
       <div class="row d-flex justify-content-center align-items-center h-100">
@@ -11,7 +16,6 @@
             alt="Sample image">
         </div>
         <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-          <form>
             <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
               <p class="lead fw-normal mb-0 me-3">Sign in with</p>
               <button type="button" class="btn btn-primary btn-floating mx-1">
@@ -28,20 +32,23 @@
             </div>
 
             <div class="divider d-flex align-items-center my-4">
-              <p class="text-center fw-bold mx-3 mb-0">Or</p>
+              <p class="text-center fw-bold mx-3 mb-0"></p>
             </div>
 
             <!-- Email input -->
             <div class="form-outline mb-4">
-              <input type="email" id="form3Example3" class="form-control form-control-lg"
-                placeholder="Enter a valid email address" name="email"/>
               <label class="form-label" for="form3Example3">Email address</label>
+              <input type="email" id="form3Example3" class="form-control form-control-lg"
+                placeholder="Enter a valid email address" name="email" value="{{old('email')}}">
+                <span class="text-danger">@error('email'){{ $message }} @enderror</span>
             </div>
             <!-- Password input -->
             <div class="form-outline mb-3">
-              <input type="password" id="form3Example4" class="form-control form-control-lg"
-                placeholder="Enter password"name="password"/>
               <label class="form-label" for="form3Example4">Password</label>
+              <input type="password" id="form3Example4" class="form-control form-control-lg"
+                placeholder="Enter password"name="password" value="{{old('password')}}">
+                <span class="text-danger">@error('password'){{ $message }} @enderror</span>
+
             </div>
             <div class="d-flex justify-content-between align-items-center">
               <!-- Checkbox -->
